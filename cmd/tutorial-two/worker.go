3e7b6be7bb831cc271cc7e1017d2cb1d66f20c4d
babygoat <babygoat1124@gmail.com>
2019-05-10 17:59:46 +0800

    [Tutorial 2] Add worker

---
+++ cmd/tutorial-two/worker.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	// version-validation and authentication to the RabbitMQ server
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// setup the real connection channel
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"task_queue", // queue name
+		true,         // durable (mesage will not be restored when the server restarts)
+		false,        // delete when unused (no consumer)
+		false,        // exclusive queue (only accessible to the declarer)
+		false,        // no-wait (?)
+		nil,          // arguments
+	)
+
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // channel-only
+	)
+	failOnError(err, "Failed to set Qos")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue name
+		"",
+		false, // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil,
+	)
+
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Recevied a message: %s", d.Body)
+			dot_count := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dot_count)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+			d.Ack(false) // Ack manually w.r.t single dispatch
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
