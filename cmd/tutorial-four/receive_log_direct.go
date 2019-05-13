a0e701ab2c19277f9338ad0acab3d46905deebba
Ching-Yang, Tseng <n7711245566@gmail.com>
2019-05-13 09:45:26 +0800

    [Tutorial four] Add direct receive logger

---
+++ cmd/tutorial-four/receive_log_direct.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	// version-validation and authentication to the RabbitMQ server
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// setup the real connection channel
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"logs_direct", // Exchange name
+		"direct",      // Exchange type
+		true,          // Durable
+		false,         // auto-deleted
+		false,         // internal
+		false,         // no-wait
+		nil,           // arguments
+	)
+
+	failOnError(err, "Failed to declare an exchange")
+
+	q, err := ch.QueueDeclare(
+		"",    // auto-generated queue name
+		false, // non-durable
+		false, // delete when unused (no consumer)
+		true,  // exclusive queue (only accessible to the declarer)
+		false, // no-wait (?)
+		nil,   // arguments
+	)
+
+	failOnError(err, "Failed to declare a queue")
+
+	if len(os.Args) < 2 {
+		log.Printf("Usage: %s [info] [warning] [error]", os.Args[0])
+		os.Exit(0)
+	}
+
+	for _, s := range os.Args[1:] {
+		log.Printf("Binding queue %s to exchange %s with routing key %s", q.Name, "logs_direct", s)
+
+		err = ch.QueueBind(
+			q.Name, // queue name
+			s,
+			"logs_direct", // exchange name
+			false,
+			nil,
+		)
+
+		failOnError(err, "Failed to bind a queue with an exchange")
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue name
+		"",
+		true,  // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil,
+	)
+
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+	<-forever
+}
