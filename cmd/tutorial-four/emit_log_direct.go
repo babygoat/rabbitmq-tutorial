125a8203b8551c3f89899989c2288cb93f15bf9e
Ching-Yang, Tseng <n7711245566@gmail.com>
2019-05-13 09:44:58 +0800

    [Tutorial four] Add direct emit logger

---
+++ cmd/tutorial-four/emit_log_direct.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func bodyFrom(args []string) string {
+	s := ""
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+
+	return s
+}
+
+func severityFrom(args []string) string {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "info"
+	} else {
+		s = os.Args[1]
+	}
+	return s
+}
+
+// Create a durable queue
+
+func main() {
+	// version-validation and authentication to the RabbitMQ server
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// setup the real connection channel
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"logs_direct", // Exchange name
+		"direct",      // Exchange type
+		true,          // Durable
+		false,         // auto-deleted
+		false,         // internal
+		false,         // no-wait
+		nil,           // arguments
+	)
+
+	failOnError(err, "Failed to declare an exchange")
+
+	body := bodyFrom(os.Args)
+
+	err = ch.Publish(
+		"logs_direct",         // exchange
+		severityFrom(os.Args), // routing key (queue name)
+		false,                 // mandatory (might not be published if no queue is bound to the exchange)
+		false,                 // immediate (might not be published if no consumer is on the matched queue)
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s", body)
+}
